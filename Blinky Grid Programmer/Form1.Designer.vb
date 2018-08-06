4c74ac59bb4f248305b65933e6a51f55edc4dbd2
Lee Adama <silverwolfjunk@gmail.com>
2018-08-05 20:46:19 -0500

    Cleaned up some Variables

--- Blinky Grid Programmer/Form1.Designer.vb
+++ Blinky Grid Programmer/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,12 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.tabMessageType = New System.Windows.Forms.TabControl()
         Me.tabPixel = New System.Windows.Forms.TabPage()
+        Me.boxPixelFrame = New System.Windows.Forms.GroupBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.cmbPixelDelay = New System.Windows.Forms.ComboBox()
         Me.radPixelAnimation = New System.Windows.Forms.RadioButton()
@@ -35,8 +36,9 @@ Partial Class Form1
         Me.txtMessage = New System.Windows.Forms.TextBox()
         Me.panExistingFrames = New System.Windows.Forms.Panel()
         Me.boxExistingFrames = New System.Windows.Forms.GroupBox()
+        Me.cmdAddMessage = New System.Windows.Forms.Button()
         Me.grpMessages = New System.Windows.Forms.GroupBox()
-        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.panMessagesToTransmit = New System.Windows.Forms.FlowLayoutPanel()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.picClock = New System.Windows.Forms.PictureBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
@@ -66,11 +68,12 @@ Partial Class Form1
         Me.tabMessageType.Location = New System.Drawing.Point(13, 13)
         Me.tabMessageType.Name = "tabMessageType"
         Me.tabMessageType.SelectedIndex = 0
-        Me.tabMessageType.Size = New System.Drawing.Size(320, 201)
+        Me.tabMessageType.Size = New System.Drawing.Size(320, 214)
         Me.tabMessageType.TabIndex = 1
         '
         'tabPixel
         '
+        Me.tabPixel.Controls.Add(Me.boxPixelFrame)
         Me.tabPixel.Controls.Add(Me.Label2)
         Me.tabPixel.Controls.Add(Me.cmbPixelDelay)
         Me.tabPixel.Controls.Add(Me.radPixelAnimation)
@@ -80,15 +83,25 @@ Partial Class Form1
         Me.tabPixel.Location = New System.Drawing.Point(4, 25)
         Me.tabPixel.Name = "tabPixel"
         Me.tabPixel.Padding = New System.Windows.Forms.Padding(3)
-        Me.tabPixel.Size = New System.Drawing.Size(312, 172)
+        Me.tabPixel.Size = New System.Drawing.Size(312, 185)
         Me.tabPixel.TabIndex = 0
         Me.tabPixel.Text = "Pixel Message"
         Me.tabPixel.UseVisualStyleBackColor = True
         '
+        'boxPixelFrame
+        '
+        Me.boxPixelFrame.Location = New System.Drawing.Point(3, 0)
+        Me.boxPixelFrame.Margin = New System.Windows.Forms.Padding(0)
+        Me.boxPixelFrame.Name = "boxPixelFrame"
+        Me.boxPixelFrame.Padding = New System.Windows.Forms.Padding(0)
+        Me.boxPixelFrame.Size = New System.Drawing.Size(168, 185)
+        Me.boxPixelFrame.TabIndex = 6
+        Me.boxPixelFrame.TabStop = False
+        '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(194, 125)
+        Me.Label2.Location = New System.Drawing.Point(183, 158)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(44, 17)
         Me.Label2.TabIndex = 5
@@ -101,7 +114,7 @@ Partial Class Form1
         Me.cmbPixelDelay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cmbPixelDelay.FormattingEnabled = True
         Me.cmbPixelDelay.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"})
-        Me.cmbPixelDelay.Location = New System.Drawing.Point(247, 122)
+        Me.cmbPixelDelay.Location = New System.Drawing.Point(247, 155)
         Me.cmbPixelDelay.MaxDropDownItems = 16
         Me.cmbPixelDelay.Name = "cmbPixelDelay"
         Me.cmbPixelDelay.Size = New System.Drawing.Size(59, 24)
@@ -111,7 +124,7 @@ Partial Class Form1
         '
         Me.radPixelAnimation.AutoSize = True
         Me.radPixelAnimation.Checked = True
-        Me.radPixelAnimation.Location = New System.Drawing.Point(197, 95)
+        Me.radPixelAnimation.Location = New System.Drawing.Point(186, 115)
         Me.radPixelAnimation.Name = "radPixelAnimation"
         Me.radPixelAnimation.Size = New System.Drawing.Size(91, 21)
         Me.radPixelAnimation.TabIndex = 3
@@ -122,7 +135,7 @@ Partial Class Form1
         'radPixelMarquee
         '
         Me.radPixelMarquee.AutoSize = True
-        Me.radPixelMarquee.Location = New System.Drawing.Point(197, 67)
+        Me.radPixelMarquee.Location = New System.Drawing.Point(186, 88)
         Me.radPixelMarquee.Name = "radPixelMarquee"
         Me.radPixelMarquee.Size = New System.Drawing.Size(85, 21)
         Me.radPixelMarquee.TabIndex = 2
@@ -131,7 +144,7 @@ Partial Class Form1
         '
         'cmdClearFrame
         '
-        Me.cmdClearFrame.Location = New System.Drawing.Point(197, 37)
+        Me.cmdClearFrame.Location = New System.Drawing.Point(186, 42)
         Me.cmdClearFrame.Name = "cmdClearFrame"
         Me.cmdClearFrame.Size = New System.Drawing.Size(102, 23)
         Me.cmdClearFrame.TabIndex = 1
@@ -140,7 +153,7 @@ Partial Class Form1
         '
         'cmdAddFrame
         '
-        Me.cmdAddFrame.Location = New System.Drawing.Point(197, 7)
+        Me.cmdAddFrame.Location = New System.Drawing.Point(186, 12)
         Me.cmdAddFrame.Name = "cmdAddFrame"
         Me.cmdAddFrame.Size = New System.Drawing.Size(102, 23)
         Me.cmdAddFrame.TabIndex = 0
@@ -153,7 +166,7 @@ Partial Class Form1
         Me.TabPage2.Location = New System.Drawing.Point(4, 25)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(312, 172)
+        Me.TabPage2.Size = New System.Drawing.Size(312, 185)
         Me.TabPage2.TabIndex = 1
         Me.TabPage2.Text = "Text Message"
         Me.TabPage2.UseVisualStyleBackColor = True
@@ -174,7 +187,7 @@ Partial Class Form1
         Me.panExistingFrames.BackColor = System.Drawing.SystemColors.ControlLightLight
         Me.panExistingFrames.Location = New System.Drawing.Point(6, 21)
         Me.panExistingFrames.Name = "panExistingFrames"
-        Me.panExistingFrames.Size = New System.Drawing.Size(675, 151)
+        Me.panExistingFrames.Size = New System.Drawing.Size(546, 186)
         Me.panExistingFrames.TabIndex = 0
         '
         'boxExistingFrames
@@ -182,39 +195,49 @@ Partial Class Form1
         Me.boxExistingFrames.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.boxExistingFrames.Controls.Add(Me.cmdAddMessage)
         Me.boxExistingFrames.Controls.Add(Me.grpMessages)
         Me.boxExistingFrames.Controls.Add(Me.panExistingFrames)
-        Me.boxExistingFrames.Location = New System.Drawing.Point(13, 220)
+        Me.boxExistingFrames.Location = New System.Drawing.Point(13, 233)
         Me.boxExistingFrames.Name = "boxExistingFrames"
-        Me.boxExistingFrames.Size = New System.Drawing.Size(687, 298)
+        Me.boxExistingFrames.Size = New System.Drawing.Size(687, 333)
         Me.boxExistingFrames.TabIndex = 2
         Me.boxExistingFrames.TabStop = False
-        Me.boxExistingFrames.Text = "Existing Frames"
+        Me.boxExistingFrames.Text = "Existing Frames (0)"
+        '
+        'cmdAddMessage
+        '
+        Me.cmdAddMessage.Location = New System.Drawing.Point(558, 21)
+        Me.cmdAddMessage.Name = "cmdAddMessage"
+        Me.cmdAddMessage.Size = New System.Drawing.Size(123, 36)
+        Me.cmdAddMessage.TabIndex = 2
+        Me.cmdAddMessage.Text = "Add Message"
+        Me.cmdAddMessage.UseVisualStyleBackColor = True
         '
         'grpMessages
         '
         Me.grpMessages.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.grpMessages.Controls.Add(Me.FlowLayoutPanel1)
-        Me.grpMessages.Location = New System.Drawing.Point(7, 179)
+        Me.grpMessages.Controls.Add(Me.panMessagesToTransmit)
+        Me.grpMessages.Location = New System.Drawing.Point(0, 214)
         Me.grpMessages.Name = "grpMessages"
-        Me.grpMessages.Size = New System.Drawing.Size(674, 113)
+        Me.grpMessages.Size = New System.Drawing.Size(681, 113)
         Me.grpMessages.TabIndex = 1
         Me.grpMessages.TabStop = False
         Me.grpMessages.Text = "Messages To Transmit (0)"
         '
-        'FlowLayoutPanel1
+        'panMessagesToTransmit
         '
-        Me.FlowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(7, 22)
-        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
-        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(661, 85)
-        Me.FlowLayoutPanel1.TabIndex = 0
+        Me.panMessagesToTransmit.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.panMessagesToTransmit.Location = New System.Drawing.Point(7, 22)
+        Me.panMessagesToTransmit.Name = "panMessagesToTransmit"
+        Me.panMessagesToTransmit.Size = New System.Drawing.Size(661, 85)
+        Me.panMessagesToTransmit.TabIndex = 0
         '
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.picClock)
-        Me.GroupBox1.Location = New System.Drawing.Point(339, 38)
+        Me.GroupBox1.Location = New System.Drawing.Point(339, 50)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(176, 148)
         Me.GroupBox1.TabIndex = 3
@@ -234,7 +257,7 @@ Partial Class Form1
         'GroupBox2
         '
         Me.GroupBox2.Controls.Add(Me.picData)
-        Me.GroupBox2.Location = New System.Drawing.Point(513, 38)
+        Me.GroupBox2.Location = New System.Drawing.Point(513, 50)
         Me.GroupBox2.Name = "GroupBox2"
         Me.GroupBox2.Size = New System.Drawing.Size(187, 148)
         Me.GroupBox2.TabIndex = 4
@@ -256,8 +279,7 @@ Partial Class Form1
         '
         'cmdTransmit
         '
-        Me.cmdTransmit.Enabled = False
-        Me.cmdTransmit.Location = New System.Drawing.Point(513, 9)
+        Me.cmdTransmit.Location = New System.Drawing.Point(513, 21)
         Me.cmdTransmit.Name = "cmdTransmit"
         Me.cmdTransmit.Size = New System.Drawing.Size(181, 23)
         Me.cmdTransmit.TabIndex = 5
@@ -267,7 +289,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(345, 12)
+        Me.Label1.Location = New System.Drawing.Point(345, 24)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(72, 17)
         Me.Label1.TabIndex = 6
@@ -275,7 +297,7 @@ Partial Class Form1
         '
         'txtDelay
         '
-        Me.txtDelay.Location = New System.Drawing.Point(423, 10)
+        Me.txtDelay.Location = New System.Drawing.Point(423, 22)
         Me.txtDelay.MaxLength = 4
         Me.txtDelay.Name = "txtDelay"
         Me.txtDelay.Size = New System.Drawing.Size(84, 22)
@@ -285,16 +307,16 @@ Partial Class Form1
         '
         'barProgress
         '
-        Me.barProgress.Location = New System.Drawing.Point(339, 191)
+        Me.barProgress.Location = New System.Drawing.Point(339, 204)
         Me.barProgress.Name = "barProgress"
-        Me.barProgress.Size = New System.Drawing.Size(349, 23)
+        Me.barProgress.Size = New System.Drawing.Size(361, 23)
         Me.barProgress.TabIndex = 1
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(712, 531)
+        Me.ClientSize = New System.Drawing.Size(712, 578)
         Me.Controls.Add(Me.barProgress)
         Me.Controls.Add(Me.txtDelay)
         Me.Controls.Add(Me.Label1)
@@ -341,6 +363,8 @@ Partial Class Form1
     Friend WithEvents radPixelMarquee As RadioButton
     Friend WithEvents txtMessage As TextBox
     Friend WithEvents grpMessages As GroupBox
-    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents panMessagesToTransmit As FlowLayoutPanel
     Friend WithEvents Label2 As Label
+    Friend WithEvents boxPixelFrame As GroupBox
+    Friend WithEvents cmdAddMessage As Button
 End Class
--- Blinky Grid Programmer/Form1.vb
+++ Blinky Grid Programmer/Form1.vb
@@ -1,27 +1,44 @@
 ﻿Imports Microsoft.VisualBasic.Devices
 
 Public Class Form1
+    'Basic Variables
     Dim IsCreated(99) As Boolean
     Dim Buttons As New Dictionary(Of String, Button)
+
+
+
+    'Pixel Frames Variables
+    Dim pixelFrames As Integer
+    Dim columnData As New List(Of Integer)
+    Dim Animation As Boolean = True
+
+
+
+    'Messages Variables
+    Dim messageData As String
+    Dim TransmitData As New List(Of Integer)
     Dim MessageInBits As String
-    Dim i As Integer
+    Dim bitCounter As Integer
+
+
 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Dim lastTop As Integer
+        Dim lastTop, i As Integer
         'Test only.  Remove from working system
         ''MessageInBits = "000011100000000000000000000001100000000100011110000010110001000100001110000101010001010100011000001010010010000000011000000110110001010100001101110000110000000000000000000000000000000111111111"
         MessageInBits = "0001000000000000000000000000011000000001110001100011000100000000000000000000000000011000000000000000000000000000000000000000000000111100001001000011110000000000001111100001000000000000000100000000011000000000000000000111111001000010010000100100001001111110000000001111111110000001100000011000000110000001100000011111111100000000100101010001000000000000001000000000011001111110010000100100001001000010011111100000000000000000000000000011110000100100001111000000000000000000000000000000000000000000011011000000010000000000001100000000011000011000000000000000000000000000101011100000000000000000000000000000000111111111"
         'MessageInBits = "0000010000000000000000000000011000000001000111100000000100010001110001010000000000000000000000000000000111111111"
         '**************************************
-        For i As Integer = 0 To 55
+        For i = 0 To 55
             Dim B As New Button
             Dim buttonKey As String
-            tabPixel.Controls.Add(B)
-            B.Height = 15
-            B.Width = 15
-            B.Left = (i Mod 7) * 16
-            B.Top = (i \ 7) * 16
+            boxPixelFrame.Controls.Add(B)
+            'B.Margin = New Padding(0)
+            B.Height = 20
+            B.Width = 20
+            B.Left = (i Mod 7) * 21 + 10
+            B.Top = (i \ 7) * 21 + 10
             lastTop = B.Top
             'B.Text = Chr((i \ 8) + Asc("A")) & i Mod 8 + 1
             buttonKey = Chr((i \ 8) + Asc("A")) & i Mod 8 + 1
@@ -31,7 +48,7 @@ Public Class Form1
         Next
 
         'Other setup items here
-        tabMessageType.Height = lastTop + 26 + 16
+        'tabMessageType.Height = lastTop + 26 + 16
         cmbPixelDelay.SelectedIndex = 5
 
 
@@ -53,10 +70,10 @@ Public Class Form1
         'Prepare a bit for transmission
         Dim length As Integer
         length = Len(MessageInBits)
-        i = i + 1
-        If i <= Len(MessageInBits) Then
+        bitCounter = bitCounter + 1
+        If bitCounter <= Len(MessageInBits) Then
             Dim bitToTransmit As Integer
-            bitToTransmit = Val(Mid(MessageInBits, i, 1))
+            bitToTransmit = Val(Mid(MessageInBits, bitCounter, 1))
             If bitToTransmit = 0 Then
                 picData.BackColor = clrBlack
             Else
@@ -113,8 +130,32 @@ Public Class Form1
         'setup stuff
         barProgress.Maximum = Len(MessageInBits) + 1
         barProgress.Value = 1
-        i = 0
+        bitCounter = 0
         tmrTransmitData.Enabled = True
 
     End Sub
+
+    Private Sub cmdAddFrame_Click(sender As Object, e As EventArgs) Handles cmdAddFrame.Click
+        'Put Frame Image in Frames Box
+        Dim bmpPixelFrame As Bitmap
+        bmpPixelFrame = TakeScreenShot(boxPixelFrame)
+        Dim P As New PictureBox
+        P.Height = bmpPixelFrame.Height
+        P.Width = bmpPixelFrame.Width
+        'P.SizeMode = P.SizeMode.StretchImage
+        P.Image = bmpPixelFrame
+        panExistingFrames.Controls.Add(P)
+
+        'Save Actual Frame Data for later transmission.
+
+    End Sub
+
+    Private Function TakeScreenShot(ByVal Control As Control) As Bitmap
+        Dim tmpImg As New Bitmap(Control.Width, Control.Height)
+        Using g As Graphics = Graphics.FromImage(tmpImg)
+            g.CopyFromScreen(Control.PointToScreen(New Point(0, 0)), New Point(0, 0), New Size(Control.Width, Control.Height))
+        End Using
+        Return tmpImg
+    End Function
+
 End Class
